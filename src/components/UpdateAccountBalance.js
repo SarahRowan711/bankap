d192c22e9c6be943303e4b48a47bef2685d7d65c
Rowan, Sarah <Sarah.Rowan@viasat.com>
2022-01-10 17:49:55 -0500

    minor updates

---
+++ src/components/UpdateAccountBalance.js
@@ -0,0 +1,11 @@
+const UpdateAccountBalance = ({ onChange, isDeposit, isValid }) => {
+    const choice = ['Deposit', 'Cash Back'];
+    console.log(`ATM isDeposit: ${isDeposit}`);
+    return (
+      <label className="label huge">
+        <h3> {choice[Number(!isDeposit)]}</h3>
+        <input id="number-input" type="number" width="200" onChange={onChange}></input>
+        <input type="submit" disabled={!isValid} width="200" value="Submit" id="submit-input"></input>
+      </label>
+    );
+  };

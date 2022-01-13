874a6ba765c9bb5d6dbb4864021d3f5b32b86f7b
Rowan, Sarah <Sarah.Rowan@viasat.com>
2022-01-13 14:48:57 -0500

    finally got context routing working

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-export const UserContext = React.createContext();
+const UserContext = React.createContext();
 
+class Context extends React.Component {
+    state = {users: [{
+        name: 'user',
+        email: 'user@mail.edu',
+        password: 'secret',
+        balance: 100
+    }]}
+    render(){
+        return(
+            <UserContext.Provider value={
+                { state: this.state,
+                setUsers: (value) => this.setState({
+                    users: value })}}>
+            {this.props.children})
+            </UserContext.Provider>);
+            
+    }
+}
+
+export { UserContext };
 export default Context;

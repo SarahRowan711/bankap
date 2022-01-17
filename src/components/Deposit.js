3dbc5b9aa74c3c232e8f4cf9f539263d713dfe07
Rowan, Sarah <Sarah.Rowan@viasat.com>
2022-01-17 14:47:13 -0500

    updates to withdrawal and deposit validations

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,42 +1,118 @@
 import React from "react";
 import {Card, Form, Button} from "react-bootstrap";
 
-class Deposit extends React.Component {
-    render(){
-        return(
-            <div
-            style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)'
-            }}>
-                <Card className="text-center" style={{ width: '30rem' }}>
-                    <Card.Body>
-                        <Card.Title>Account Balance</Card.Title>
-                        <Card.Text>BALANCE GOES HERE</Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                        <Card.Title>Make a deposit to your account</Card.Title>
-                        <Card.Text>
-                            <Form>
-                                <Form.Control type="number" placeholder="Enter your deposit amount" onChange={onChange} />
-                            </Form>
-                        </Card.Text>
-                        <Button className="btn-custom" type="submit" onClick={submitDeposit}>
-                            Submit Deposit
-                        </Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
+function Deposit() {
+    const[deposit, setDeposit] = React.useState();
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState('');
+    //fetch balance from context
+    //faking for now
+    let balance = 500;
+    
+    function validate(field, label){
+        if (!field) {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
     }
-};
 
-const onChange = (onChange) => {
-    return (console.log('onChangeblah'));
-}
-const submitDeposit = (submitDeposit) => {
-    return (console.log('submitDepositblah'));
-}
+    function validateNumber(field) {
+        if (isNaN(parseFloat(field))) {
+            setStatus('Error');
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    function validatePositiveNumber(field) {
+        if (Number(field) < 0) {
+            setStatus('Error');
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleDeposit() {
+        //validate that input was entered
+        if(!validate(deposit, 'deposit')) {
+            alert('Deposit amount is required');
+            return;
+        }
+
+        //validate that input is a number
+        if(!validateNumber(deposit)) {
+            alert('Deposit amount must be a number');
+            return;
+        }
+
+        //validate that input is not negative
+        if(!validatePositiveNumber(deposit)) {
+            alert('Deposit amount cannot be negative');
+            return;
+        }
+   
+        //add deposit to existing balance
+        let newBalance = balance + Number(deposit);
+        
+        console.log('new balance is ' + newBalance);
+        setShow(false);
+        return newBalance;
+        }
+
+    function clearForm(){
+        setDeposit('');
+        setShow(true);
+    }
+
+
+    return(
+        <div
+        style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <Card className="text-center" style={{ width: '30rem' }}>
+                <Card.Body>
+                    <Card.Title>Account Balance</Card.Title>
+                    <Card.Text>{balance}</Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    {show
+                    ?
+                    (    
+                    <>
+                    <Card.Title>Make a deposit to your account</Card.Title>
+                    <Card.Text>
+                        <Form>
+                            <Form.Control type="text" value={deposit} 
+                            placeholder="Enter your deposit amount" 
+                            onChange={e => setDeposit(e.currentTarget.value)} />
+                        </Form>
+                    </Card.Text>
+                    <Button className="btn-custom" type="submit" onClick={handleDeposit}>
+                        Submit Deposit
+                    </Button>
+                    </>
+                    ):(
+                    <>
+                    <span>
+                    <h5><center>Your deposit has been submitted successfuly!</center></h5>
+                        <Button className="btn-custom"
+                        onClick={clearForm}>
+                            Submit Another Deposit
+                        </Button>
+                    </span>
+                    </>)
+                    }
+                </Card.Body>
+            </Card>
+        </div>
+        )
+    };
 
 
 
@@ -46,6 +122,6 @@ const submitDeposit = (submitDeposit) => {
 //success message
 //styled as a card
 //display updated balance after deposit, success message
-//validate number and only number is entered, disable button is validation fails
+//validate number and only number is entered, disable button if validation fails
 
 export default Deposit;
--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -1,34 +1,119 @@
 import React from "react";
 import {Card, Form, Button} from "react-bootstrap";
 
-class Withdraw extends React.Component {
-    render(){
-        return(
-            <div
-            style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)'
-            }}>
-                <Card className="text-center" style={{ width: '30rem' }}>
-                    <Card.Body>
-                        <Card.Title>Account Balance</Card.Title>
-                        <Card.Text>BALANCE GOES HERE</Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                        <Card.Title>Make a withdrawal from your account</Card.Title>
-                        <Card.Text>
-                            <Form>
-                                <Form.Control type="number" placeholder="Enter your withdrawal amount" />
-                            </Form>
-                        </Card.Text>
-                        <Button className="btn-custom" type="submit">
-                            Submit Withdrawal
-                        </Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
+function Withdraw() {
+    const[withdrawal, setWithdrawal] = React.useState();
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState('');
+    //fetch balance from context
+    //faking for now
+    let balance = 500;
+    
+    function validate(field, label){
+        if (!field) {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
     }
+
+    function validateNumber(field) {
+        if (isNaN(parseFloat(field))) {
+            setStatus('Error');
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    function validatePositiveNumber(field) {
+        if (Number(field) < 0) {
+            setStatus('Error');
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    //validate withdrawal won't cause negative balance
+
+    function handleWithdraw() {
+        //validate that input was entered
+        if(!validate(withdrawal, 'withdrawal')) {
+            alert('Withdrawal amount is required');
+            return;
+        }
+
+        //validate that input is a number
+        if(!validateNumber(withdrawal)) {
+            alert('Withdrawal amount must be a number');
+            return;
+        }
+
+        //validate that input is not negative
+        if(!validatePositiveNumber(withdrawal)) {
+            alert('Withdrawal amount cannot be negative');
+            return;
+        }
+    
+        //subtract withdrawal from existing balance
+        let newBalance = balance - Number(withdrawal);
+        
+        console.log('new balance is ' + newBalance);
+        setShow(false);
+        return newBalance;
+        }
+
+    function clearForm(){
+        setWithdrawal('');
+        setShow(true);
+    }
+
+
+    return(
+        <div
+        style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <Card className="text-center" style={{ width: '30rem' }}>
+                <Card.Body>
+                    <Card.Title>Account Balance</Card.Title>
+                    <Card.Text>{balance}</Card.Text>
+                </Card.Body>
+                <Card.Body>
+                {show
+                    ?
+                    (    
+                    <>
+                    <Card.Title>Make a withdrawal from your account</Card.Title>
+                    <Card.Text>
+                        <Form>
+                            <Form.Control type="text" value={withdrawal} 
+                            placeholder="Enter your withdrawal amount" 
+                            onChange={e => setWithdrawal(e.currentTarget.value)} />
+                        </Form>
+                    </Card.Text>
+                    <Button className="btn-custom" type="submit" onClick={handleWithdraw}>
+                        Submit Withdrawal
+                    </Button>
+                    </>
+                    ):(
+                    <>
+                    <span>
+                    <h5><center>Your withdrawal has been submitted successfully!</center></h5>
+                        <Button className="btn-custom"
+                        onClick={clearForm}>
+                            Submit Another Withdrawal
+                        </Button>
+                    </span>
+                    </>)
+                    }
+                </Card.Body>
+            </Card>
+        </div>
+    )
 };
 
 function withdraw(){
@@ -36,11 +121,11 @@ function withdraw(){
 }
 
 //balance display
-//deposit input
+//withdrawal input
 //submit button
 //success message
 //styled as a card
-//display updated balance after deposit, success message 
+//display updated balance after withdrawal, success message 
 //negative balance alert - disable submit button
 //validate number and only number is entered, disable button if validation fails
 
